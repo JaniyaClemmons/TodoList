49091e0be6f98271a0c70587da303466a074398c
Janiya Clemmons <janiya.c4lyfe@yahoo.com>
2023-11-10 12:34:58 +0100

    backend CRUD working with postman

--- spring-boot-todo-backend/src/main/java/com/jcdev/springboottodobackend/controller/TodoController.java
+++ spring-boot-todo-backend/src/main/java/com/jcdev/springboottodobackend/controller/TodoController.java
@@ -10,8 +10,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,14 +43,53 @@ public class TodoController {
 
     }
 
-    // GET all todo items from db
+    // GET todo item by id
     @GetMapping("/{id}")
     public ResponseEntity<?> fetchTodoItem(@PathVariable("id") Long id) {
 
-        logger.info("request to GET one item with id: " + id);
+        logger.info("request to GET item with id: " + id);
         TodoItem todoItem = todoService.fetchTodoItem(id);
 
         return ResponseEntity.status(HttpStatus.OK).body(todoItem);
+
+    }
+
+    // POST todo item
+    @PostMapping("")
+    public ResponseEntity<?> createTodoItem(@RequestBody TodoItem todoItem) {
+
+        logger.info("request to POST new item ");
+        TodoItem newItem = todoService.createTodoItem(todoItem);
+
+        return ResponseEntity.status(HttpStatus.OK).body(newItem);
+
+    }
+
+    // UPDATE todo item by id
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateTodoItem(@PathVariable("id") Long id, @RequestBody TodoItem todoItem) {
+
+        logger.info("request to UPDATE item with id: " + id);
+        TodoItem newItem = todoService.updateTodoItem(id, todoItem);
+        if (newItem == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No item found with id: " + id);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(newItem);
+
+    }
+
+    // DELETE todo item by id
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteTodoItem(@PathVariable("id") Long id) {
+
+        logger.info("request to DELETE item with id: " + id);
+        TodoItem newItem = todoService.deleteTodoItem(id);
+        if (newItem == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No item found with id: " + id);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(newItem);
 
     }
 
--- spring-boot-todo-backend/src/main/java/com/jcdev/springboottodobackend/service/ToDoService.java
+++ spring-boot-todo-backend/src/main/java/com/jcdev/springboottodobackend/service/ToDoService.java
@@ -25,12 +25,38 @@ public class TodoService {
         if (todoOpt.isPresent()) {
             TodoItem item = todoOpt.get();
             return item;
-            
+        }
+        return null;
+    }
+
+    public TodoItem createTodoItem(TodoItem todoItem) {
+        TodoItem newItem = new TodoItem(todoItem.getTask());
+
+        return todoRepository.save(newItem);
+    }
+
+    public TodoItem updateTodoItem(Long id, TodoItem todoItem) {
+        Optional<TodoItem> todoOpt = todoRepository.findById(id);
+        if (todoOpt.isPresent()) {
+            TodoItem item = todoOpt.get();
+            item.setIsComplete(todoItem.getIsComplete());
+            item.setTask(todoItem.getTask());
+            todoRepository.save(item);
+            return item;
+
+        }
+        return null;
+    }
+
+    public TodoItem deleteTodoItem(Long id) {
+        try {
+            TodoItem todoItem = fetchTodoItem(id);
+            todoRepository.deleteById(id);
+            return todoItem;
+        } catch (IllegalArgumentException i) {
+            return null;
         }
 
-        return null;
-
-        
     }
 
 }
